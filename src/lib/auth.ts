5a8822a80d136fd270977ba71f5f6b5f03fb5650
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-09-05 18:51:58 +0200

    9. Hooks (password validation on backend)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { createAuthMiddleware, APIError } from "better-auth/api";
 import prisma from "@/lib/prisma";
+import { passwordSchema } from "./validation";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -16,6 +18,24 @@ export const auth = betterAuth({
         input: false,
       },
     },
+  },
+  hooks: {
+    before: createAuthMiddleware(async (c) => {
+      if (
+        c.path === "/sign-up/email" ||
+        c.path === "/reset-password" ||
+        c.path === "/change-password"
+      ) {
+        const password = c.body.password || c.body.newPassword;
+        const { success, error } = passwordSchema.safeParse(password);
+
+        if (error) {
+          throw new APIError("BAD_REQUEST", {
+            message: "Password not strong enough",
+          });
+        }
+      }
+    }),
   },
 });
 
